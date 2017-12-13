e4d61e0f89814ec8cff4728744acbb1bb3f63f2d
Primoz <primoz969@gmail.com>
2017-12-13 21:05:58 +0100

    playing with javascript

--- js.js
+++ js.js
@@ -16,3 +16,79 @@ document.getElementById("button4").onclick = function () {
     location.href = "sub_page_Ls.html";
 };
 
+
+
+
+var img1 = document.getElementById("img1");
+var img2 = document.getElementById("img2");
+var img3 = document.getElementById("img3");
+var img4 = document.getElementById("img4");
+
+var btn1 = document.getElementById("button1");
+var btn2 = document.getElementById("button2");
+var btn3 = document.getElementById("button3");
+var btn4 = document.getElementById("button4");
+
+var chosen = "chosen.png";
+var notChosen = "notchosen.png";
+
+document.getElementById("img1").src="Cornelia_.jpg";
+btn1.addEventListener("mouseover", function(){
+	
+	img2.src = "notchosen.png";
+	img3.src = "notchosen.png";
+	img4.src = "notchosen.png";
+
+	btn1.addEventListener("mouseout", function(){
+		img1.src = "Cornelia_.jpg";
+		img2.src = "Primoz.jpg";
+		img3.src = "Qais.jpg";
+		img4.src = "lidye.jpg";
+	});
+});
+
+
+btn2.addEventListener("mouseover", function(){
+	img1.src = "notchosen.png";
+	img3.src = "notchosen.png";
+	img4.src = "notchosen.png";
+
+	btn2.addEventListener("mouseout", function(){
+		img1.src = "Cornelia_.jpg";
+		img2.src = "Primoz.jpg";
+		img3.src = "Qais.jpg";
+		img4.src = "lidye.jpg";
+	});
+});
+
+
+btn3.addEventListener("mouseover", function(){
+	img1.src = "notchosen.png";
+	img2.src = "notchosen.png";
+	img4.src = "notchosen.png";
+
+	btn3.addEventListener("mouseout", function(){
+		img1.src = "Cornelia_.jpg";
+		img2.src = "Primoz.jpg";
+		img3.src = "Qais.jpg";
+		img4.src = "lidye.jpg";
+	});
+});
+
+
+btn4.addEventListener("mouseover", function(){
+	img1.src = "notchosen.png";
+	img2.src = "notchosen.png";
+	img3.src = "notchosen.png";
+
+	btn4.addEventListener("mouseout", function(){
+		img1.src = "Cornelia_.jpg";
+		img2.src = "Primoz.jpg";
+		img3.src = "Qais.jpg";
+		img4.src = "lidye.jpg";
+	});
+	
+});
+
+
+
